78e3a3b5856fb333d268d511be06d322f1b2a7dc
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2024-03-01 00:25:14 -0500

    Update relays.ts

--- constants/relays.ts
+++ constants/relays.ts
@@ -1,9 +1,9 @@
 export const RELAYS = [
   "wss://relay.plebstr.com",
   "wss://nos.bitcoiner.social",
-  "wss://nostr.wine",
-  "wss://offchain.pub",
-  "wss://nostr.swiss-enigma.ch",
+  "wss://nostr.myshosholoza.co.za",
+  "wss://nostr.lnproxy.org",
+  "wss://relay.snort.social",
   "wss://relay.nostr.band",
-  "wss://relayable.org",
+  "wss://nostr.hifish.org",
 ];
